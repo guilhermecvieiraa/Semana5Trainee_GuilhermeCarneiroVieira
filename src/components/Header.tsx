3601c56543ad7e4b22167d3883594eeb694baef2
Guilherme Carneiro Vieira <guilherme.carneirovieira17@gmail.com>
2026-03-08 23:35:25 -0300

    style(Header): ajusta do título.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header(){
     return(
         <div>
-            <h1 className="text-3xl text-blue-500 font-bold text-center p-6">iRepair</h1>
-        </div>
+            <h1 className="text-3xl text-blue-500 font-bold text-center p-6"> iRepair </h1>
+        </div> 
     )
 }
 
