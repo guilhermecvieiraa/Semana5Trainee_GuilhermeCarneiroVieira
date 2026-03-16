34be5f7c257b2e77378e96794e523d25081998a6
Guilherme Carneiro Vieira <guilherme.carneirovieira17@gmail.com>
2026-03-15 21:36:37 -0300

    feat(App.tsx): edita o App.tsx usar o React Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import NewServiceForm from "./components/NewServiceForm"
-import type { questionario } from "./components/NewServiceForm"
-import ServiceCard from "./components/ServiceCard"
+import {Routes, Route} from 'react-router'
+import MainLayout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Clients from './pages/clients'
+import ServiceOrders from './pages/service-orders'
 
-function App() {
-
-    const [servicos, setServicos] = useState<questionario[]>([])
-
-    function adicionarServico(novoServico: questionario){
-      setServicos([...servicos, novoServico])
-    }
-
-    function alterarStatus(index: number, novoStatus: string){
-      const novaLista = [...servicos]
-      novaLista[index].status = novoStatus
-      setServicos(novaLista)
-    }
-
-  return (
-    <div className="max-w-3xl mx-auto p-4">
-      <Header />
-      <NewServiceForm onAddService={adicionarServico} />
-      <div className="mt-8">
-        <h2 className="text-xl font-bold mb-4 text-center">Serviços Cadastrados</h2>
+const App = () => {
+  return(
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
         
-        {servicos.map((servico, index) => (
-          <ServiceCard key={index} index={index} servico={servico} mudarStatus={alterarStatus}/>
-        ))}
-      </div>
-
-    </div>
+        <Route index element={<Dashboard />} />
+        
+        <Route path="clients" element={<Clients />} />
+        
+        <Route path="service-orders" element={<ServiceOrders />} />
+        
+      </Route>
+    </Routes>
   )
 }
 
